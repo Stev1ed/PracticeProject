9558114955857b92f2dd564c248e7352493070cb
Stephen Dowdall <dowdallstephen@gmail.com>
2017-08-13 13:23:15 +0000

    first commit

---
+++ eshop/config/routes.rb
@@ -0,0 +1,45 @@
+Rails.application.routes.draw do
+  devise_for :'users'
+  
+  get 'cart/controller'
+
+  get 'site/about'
+    get'/about'=>'site#about'
+
+  get 'site/contact'
+    get'/contact'=>'site#contact'
+
+  get 'site/home'
+    get'/home'=>'site#home'
+  
+  resources :items
+  # For details on the DSL available witclearhin this file, see http://guides.rubyonrails.org/routing.html
+  
+  get"cart/index"
+  
+  get "site/about"
+  
+  get "site/contact"
+  
+  resources :items
+  
+  get '/cart' => 'cart#index'
+  
+  get '/cart/:id' => 'cart#add'
+  
+  get '/about' => 'site#about'
+  
+  get '/contact' => 'site#contact'
+  
+  get '/cart/remove/:id' =>'cart#remove'
+  
+  get '/clearCart' => 'cart#clearCart'
+  
+  get 'items' => 'items#index'
+  
+
+  
+  
+  root :to => 'site#home'
+
+end
